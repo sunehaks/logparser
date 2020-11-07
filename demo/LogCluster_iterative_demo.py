b20c07274cd36f9988d21b49088d42a277cbc29d
Suneha <suneha@vunet.local>
2020-11-07 11:35:42 +0530

    Clean LogCluster_iterative code

--- demo/LogCluster_iterative_demo.py
+++ demo/LogCluster_iterative_demo.py
@@ -5,20 +5,15 @@ from logparser.LogCluster import LogCluster_iterative
 
 # Hyperparameter tuning for cisco_router.log :
 numIterations = 27 # Hyperparameter that needs to be tuned.
-rsupports = [23.9, 23, 9, 9, 9, 8, 8, 8, 7, 26.7, 7, 7, 8, 7, 10.1, 7, 5, 9, 5,7,5, 8.6, 5, 8, 5, 5]
-initial_support = 20
+rsupports = [23.9, 23, 9, 9, 9, 8, 8, 8, 7, 26.7, 7, 7, 8, 7, 10.1, 7, 5, 9, 5,7,5, 8.6, 5, 8, 5, 5] # Hyperparameter that needs to be tuned.
+initial_support = 20 # Hyperparameter that needs to be tuned.
 file_name = "cisco_router.log"
 
-# Hyperparameter tuning for 'fin-transaction_log_anonimized.log'
-# numIterations = 9
-# rsupports = [2.7, 2.95, 2, 2, 2, 1.47, 3.18, 2.3]
-# file_name = 'fin-transaction_log_anonimized.log'
-# initial_support = 2.29
 for iteration in range(1,numIterations+1):
     if iteration==1:
         input_dir  = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+'/logs/Sample_logs/'  # The input directory of log file
-        output_dir = 'LogCluster_result/financial_transaction_results/Iterative_results'#cisco_router_results/Iterative_results' # The output directory of parsing results
-        log_file   = file_name#'cisco_router.log'
+        output_dir = 'LogCluster_result/financial_transaction_results/Iterative_results' # The output directory of parsing results
+        log_file   = file_name
         log_format = '<Date> <Time> <Level> <Router> <Pid>: <Month> <Day> <UTCTime>: <Component>: <Content>' # cisco_router log format
 #        log_format = '<Date> <Time> <Level> <Module> \[<StatusAndPayThread>\] - <Content>'
         rsupport   = initial_support # The minimum threshold of relative support, 10 denotes 10%
@@ -28,7 +23,7 @@ for iteration in range(1,numIterations+1):
         output_dir = 'LogCluster_result/financial_transaction_results/Iterative_results'#cisco_router_results/Iterative_results' # The output directory of parsing results
         log_file   = 'logcluster_input.log'
         log_format = '<Content>'
-        rsupport   = rsupports[iteration-2] # The minimum threshold of relative support, 10 denotes 10%
+        rsupport   = rsupports[iteration-2]
         regex      = []
 
     print("Iteration : ",iteration)
